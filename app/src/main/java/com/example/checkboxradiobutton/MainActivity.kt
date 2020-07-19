3f7d50297e616cfe260d3ffdfe66189a072ef86f
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-07-19 16:03:03 +0100

    add routine to prepare order list on button click

--- app/src/main/java/com/example/checkboxradiobutton/MainActivity.kt
+++ app/src/main/java/com/example/checkboxradiobutton/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.checkboxradiobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View.VISIBLE
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnOrder.setOnClickListener{
+            var checkedOptionId=rgChoice.checkedRadioButtonId
+            var selectedOpton=findViewById<RadioButton>(checkedOptionId)
+            var onion:Boolean=chkOnion.isChecked
+            var cheese:Boolean=chkCheese.isChecked
+            var salad:Boolean=chkSalad.isChecked
+            tvOrder.visibility= VISIBLE
+            tvOrder.text="You ordered the following \n " +
+            "${selectedOpton.text.toString()}" +
+                    (if(onion) "\nOnion" else "") +
+                    (if(cheese) "\nCheese" else "") +
+                    (if(salad) "\nSalad" else "")
+        }
+
+
     }
 }
